dc1422da8767562fd6c13524b097985e5c017723
Brad Slagle <btslagle@gmail.com>
2022-06-10 17:18:36 -0500

    Get Subscribe to work

--- src/app/vehicle.service.ts
+++ src/app/vehicle.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { Vehicle } from './models/Vehicle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
 
+  vehicles: Vehicle[] = [{
+    imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHQh96n6xOf8aBX1UpfateV-6aZAu77jgk7Q&usqp=CAU",
+    make:"Make: Ford", 
+    model:"Model: Mustang",
+    year:"Year: 2023",
+    condition: "Condition: Good",
+    term:"Remanning Term: 13 months",
+    monthly: "Monthly Payment: $650.37",
+    mileage:"Mileage: 30,000", 
+    location:"Location: Scottsdale, AZ 85054",
+  },{
+    imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHQh96n6xOf8aBX1UpfateV-6aZAu77jgk7Q&usqp=CAU",
+    make:"Make: Ford", 
+    model:"Model: Mustang",
+    year:"Year: 2023",
+    condition: "Condition: Good",
+    term:"Remanning Term: 13 months",
+    monthly: "Monthly Payment: $650.37",
+    mileage:"Mileage: 30,000", 
+    location:"Location: Scottsdale, AZ 85054",
+  },{
+    imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHQh96n6xOf8aBX1UpfateV-6aZAu77jgk7Q&usqp=CAU",
+    make:"Make: Ford", 
+    model:"Model: Mustang",
+    year:"Year: 2023",
+    condition: "Condition: Good",
+    term:"Remanning Term: 13 months",
+    monthly: "Monthly Payment: $650.37",
+    mileage:"Mileage: 30,000", 
+    location:"Location: Scottsdale, AZ 85054",
+  }]
+
+    fetchVehicles(){
+      return of(this.vehicles)
+    }
+  
+
   constructor() { }
 }
+
+
--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,5 +1,6 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Vehicle } from '../models/Vehicle';
+import { VehicleService } from '../vehicle.service';
 
 
 @Component({
@@ -7,29 +8,17 @@ import { Vehicle } from '../models/Vehicle';
   templateUrl: './vehicles.component.html',
   styleUrls: ['./vehicles.component.css']
 })
-export class VehiclesComponent {
-  vehicles: Vehicle[] = [{
-    imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHQh96n6xOf8aBX1UpfateV-6aZAu77jgk7Q&usqp=CAU",
-    make:"Make: Ford", 
-    model:"Model: Mustang",
-    year:"Year: 2023",
-    condition: "Condition: Good",
-    term:"Remanning Term: 13 months",
-    monthly: "Monthly Payment: $650.37",
-    mileage:"Mileage: 30,000", 
-    location:"Location: Scottsdale, AZ 85054",
-  },{
-    imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHQh96n6xOf8aBX1UpfateV-6aZAu77jgk7Q&usqp=CAU",
-    make:"Make: Ford", 
-    model:"Model: Mustang",
-    year:"Year: 2023",
-    condition: "Condition: Good",
-    term:"Remanning Term: 13 months",
-    monthly: "Monthly Payment: $650.37",
-    mileage:"Mileage: 30,000", 
-    location:"Location: Scottsdale, AZ 85054",
-  }]
-  
+export class VehiclesComponent implements OnInit {
+  vehicles: Vehicle[] = []
+
+  constructor(private vehicleService: VehicleService){}
+
+  ngOnInit(): void {
+      this.vehicleService.fetchVehicles().subscribe(vehicles =>{
+      this.vehicles = vehicles
+
+    }) 
+  }
 
 
 }
