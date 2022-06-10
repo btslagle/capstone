be93c3a46105f5f01f7130805ddecb233e1fab6d
Brad Slagle <btslagle@gmail.com>
2022-06-10 17:00:26 -0500

    Make NgFor work

---
+++ src/app/models/Vehicle.ts
@@ -0,0 +1,11 @@
+export type Vehicle ={
+    imageUrl: string;
+    make: string;
+    model: string;
+    year: string;
+    condition: string;
+    term: string;
+    monthly: string
+    mileage: string;
+    location:string;
+}
--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { Vehicle } from '../models/Vehicle';
+
 
 @Component({
   selector: 'app-vehicles',
@@ -6,7 +8,27 @@ import { Component} from '@angular/core';
   styleUrls: ['./vehicles.component.css']
 })
 export class VehiclesComponent {
-
+  vehicles: Vehicle[] = [{
+    imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHQh96n6xOf8aBX1UpfateV-6aZAu77jgk7Q&usqp=CAU",
+    make:"Make: Ford", 
+    model:"Model: Mustang",
+    year:"Year: 2023",
+    condition: "Condition: Good",
+    term:"Remanning Term: 13 months",
+    monthly: "Monthly Payment: $650.37",
+    mileage:"Mileage: 30,000", 
+    location:"Location: Scottsdale, AZ 85054",
+  },{
+    imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHQh96n6xOf8aBX1UpfateV-6aZAu77jgk7Q&usqp=CAU",
+    make:"Make: Ford", 
+    model:"Model: Mustang",
+    year:"Year: 2023",
+    condition: "Condition: Good",
+    term:"Remanning Term: 13 months",
+    monthly: "Monthly Payment: $650.37",
+    mileage:"Mileage: 30,000", 
+    location:"Location: Scottsdale, AZ 85054",
+  }]
   
 
 
